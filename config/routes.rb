f08625baeb8f30750db4eef0ae9634cde35be671
Yury Kotov <bairkan@gmail.com>
2015-02-03 00:46:55 +0400

    User controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  delete 'sign_out' => 'user_sessions#destroy', as: 'sign_out'
+  get 'sign_in' => 'user_sessions#new', as: 'sign_in'
+
+  resources :user_sessions, only: :create
+  resources :users, only: :create
+
   root 'trainings#show'
 end
