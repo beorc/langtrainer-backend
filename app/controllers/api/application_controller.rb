c91e25ed2f6659fe6ec50bf02ce204b2903ccc2e
Yury Kotov <bairkan@gmail.com>
2015-02-07 11:01:09 +0400

    Fixing problem with options request

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -12,7 +12,7 @@ class Api::ApplicationController < ApplicationController
 
   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS?redirectlocale=en-US&redirectslug=HTTP_access_control
   def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Origin'] = 'http://training.langtrainer.com'
     headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, X-Requested-With, X-Prototype-Version, X-CSRF-Token'
     headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     headers['Access-Control-Max-Age'] = '1728000'
