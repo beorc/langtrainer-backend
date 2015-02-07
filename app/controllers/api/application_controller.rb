b90b3134ebcc74b847eed6c18aa334df5f280336
Yury Kotov <bairkan@gmail.com>
2015-02-07 10:56:16 +0400

    Fixing problem with options request

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -13,9 +13,8 @@ class Api::ApplicationController < ApplicationController
   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS?redirectlocale=en-US&redirectslug=HTTP_access_control
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Allow-Methods'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, X-Requested-With, X-Prototype-Version, X-CSRF-Token'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     headers['Access-Control-Max-Age'] = '1728000'
     headers['X-Frame-Options'] = 'GOFORIT'
   end
