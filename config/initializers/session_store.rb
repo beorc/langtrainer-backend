2a16a7156de2b4c88418bc7c71f805ad313c79b6
Yury Kotov <bairkan@gmail.com>
2015-02-09 11:53:33 +0400

    Memcached for session storage

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_langtrainer_backend_session'
+#Rails.application.config.session_store :cookie_store, key: '_langtrainer_backend_session'
+#Rails.application.config.session_store :redis_store, "#{ENV['REDISTOGO_URL']}/sessions".gsub('//', '/'), { expires_in: 90.minutes }
+Rails.application.config.session_store = :dalli_store,
+  (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+  {:username => ENV["MEMCACHIER_USERNAME"],
+   :password => ENV["MEMCACHIER_PASSWORD"],
+   :failover => true,
+   :socket_timeout => 1.5,
+   :socket_failure_delay => 0.2,
+   :namespace => 'sessions',
+   :expires_in => 1.week
+  }
